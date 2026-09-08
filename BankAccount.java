910490cbdc2aa9223330cd2847c6fd3ea3af90dd
Joshua Lynds <joshua@joshuas-mbp.mynetworksettings.com>
2026-09-07 22:30:49 -0400

    did the TODO in the homework and now staging

---
+++ BankAccount.java
@@ -0,0 +1,35 @@
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+public BankAccount(String accountHolder, double balance) {
+    this.accountHolder = accountHolder;
+    this.balance = balance;
+ }
+
+public void deposit(double amount) {
+// TODO: add the amount to the balance
+    if (amount < 1){
+        System.out.println("Nothing was added to balance.");
+        return;
+    }
+    balance += amount;
+}
+
+public boolean withdraw(double amount) {
+// TODO:
+// Withdraw the requested amount if sufficient funds are available.
+// Return true if the withdrawal succeeds.
+// Return false otherwise. return false;
+    if (amount <= balance){
+        balance = balance - amount;
+        return true;
+    }
+    return false;
+}
+
+public String getAccountSummary() {
+
+    return accountHolder + ": $" + balance;
+}
+}
